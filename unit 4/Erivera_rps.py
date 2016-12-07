c77749548f76cc5dd4eec7c4c52b9cbc246e4187
ethanriverauag <ethanrivera@uagateway.org>
2016-12-07 17:14:40 +0000

    Worked on this

--- unit 4/Erivera_rps.py
+++ unit 4/Erivera_rps.py
@@ -5,7 +5,7 @@ import random
 #   purpose: present player with options, use input() to get player move
 #   returns: the player's move as either 'r', 'p', or 's'
 def get_p1_move():
-    move=input("r,p,s: ") 
+    move=input("rock,paper,scissors: ") 
     return move
 
 #function name: get_comp_move():
@@ -23,12 +23,14 @@ def get_comp_move():
       print("scissors")
     return randy
 
+
 #function name: get_rounds
 #   arguments: none
 #   purpose: allows the user to choose a number of rounds from 1 to 9.
 #   returns: the user-chosen number of rounds
 def get_rounds():
-    #code here
+    rounds=int(input("How many rounds do you want to play 1-9: "))
+    return rounds
 
 #function name: get_round_winner
 #   arguments: player move, computer move
@@ -38,40 +40,61 @@ def get_rounds():
 #               "player" if player won
 #               "comp" if computer won
 #               "tie" if it's a tie
-    def get_round_winner(p1move, cmove):
-    #code here
+def get_round_winner(p1move, cmove):
+        if p1move == cmove:
+            return("Tie!")
+        if p1move == "rock" and cmove == "paper":
+            return("You lose!")
+        elif p1move =="paper" and cmove == "rock":
+            return("You win!")
+        elif p1move =="scissors" and cmove == "rock":
+            return("You lose!")
+        elif p1move =="rock" and cmove == "scissors":
+            return("You win!")
+        elif p1move =="scissors" and cmove == "paper":
+            return("You win!")
+        elif p1move =="paper" and cmove == "scissors":
+            return("You lose!")
 
-#function name: get_full_move
-#   arguments: a single letter move 'r','p', or 's'
-#   purpose: returns the "full" word of a given move
-#   returns: returns a string based on the following:
-#               "Rock" if given "r"
-#               "Paper" if given "p"
-#               "Scissors" if given "s"
-     def get_full_move(shortmove):
-    #code here
+
+
+
 
 #function name: print_score
 #   arguments: player score, computer score, number of ties
 #   purpose: prints the scoreboard
 #   returns: none
-        def print_score(pscore, cscore, ties):
-    #code here
+def print_score(pscore, cscore, ties):
+    print("Player score:{}".format(pscore))
+    print("Computer score:{}".format(cscore))
+    print("Tie score:{}".format(ties))
+        
 
 #function name: rps
 #   arguments: none
 #   purpose: the main game loop.  This should be the longest, using
 #               all the other functions to create RPS
 #   returns: none
-            def rps():
-    #code here
-
-#function name: tests
-#   arguments: none
-#   purpose: a place for you to write your tests.  replace 'rps' below
-#               with 'tests' to run this function instead of the game loop
-#   returns: none
-                def test():
-    #code here
-
-                    rps()
+def rps():
+    pscore=0
+    cscore=0
+    ties=0
+    rounds=get_rounds()
+    
+    for rounds in range(1, rounds + 1):
+        p1move=get_p1_move()
+        randy = get_comp_move()
+        winner = get_round_winner(p1move,randy)
+        print("Computer chose {}".format(randy))
+        if winner == "You Win!":
+            print("Player Won!")
+            pscore= pscore+1
+        elif winner == "You lose!":
+            print("Computer Won!")
+            cscore = cscore+1
+        else:
+            print("It's a tie!")
+            ties= ties+1
+        print_score(pscore,cscore,ties)
+rps()
+    

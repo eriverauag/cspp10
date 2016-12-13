6a74c5fba69c474734906159904f06ea7bd84ebe
ethanriverauag <ethanrivera@uagateway.org>
2016-12-13 17:15:33 +0000

    Worked on this lab

--- unit 4/Erivera_craps.py
+++ unit 4/Erivera_craps.py
@@ -1,19 +1,28 @@
 import random
+
+def get_bet(bank_account): 
+    while True:
+        bet=int(input("How much would you like to bet?: $"))
+        if bet<0:
+            return("Your bet must be a postitive interger higher than $0")
+        elif bet>100:
+            return("You only have $100 to bet, you can't bet anymore!")
+        else:
+            return bet
+    
+    return bet
 def roll2dice():
-    dice1 = random.randint(1,6)
-    dice2 = random.randint(1,6)
+    dice1= random.randint(1,6)
+    dice2= random.randint(1,6)
     dice_sum = dice1 + dice2
-    print("Rolled 2 dice: {} {}".format(dice1,dice2))
+    print("Rolled 2 dice:{}{}".format(dice1,dice2))
     return dice_sum
-def roll_result():
-    dice1 + dice2 = roll_result()
-    if roll_result() == "7,11": 
-        return "Win"
-    elif roll_result() == "2,3,12":
-        return "Lose"
+
+def first_roll_result(dice_sum):
+    if dice_sum == 7 or dice_sum == 11:
+        return("You win!")
+    if dice_sum == 2 or dice_sum == 3 or dice_sum == 12:
+        return dice_sum
     else:
-        return "Point"
-def bet+money(bank_account):
-    bet=int(input("How much money do you want to bet?"))
-    if bet <0:
-        return
+        return dice_sum
+        

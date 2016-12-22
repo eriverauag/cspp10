f8d0c8d3f467f98f06ec6ffd423cb6c6b831cbe1
ethanriverauag <ethanrivera@uagateway.org>
2016-12-22 17:15:17 +0000

    Worked on lab

--- unit4/Erivera_craps.py
+++ unit4/Erivera_craps.py
@@ -22,20 +22,22 @@ def roll2dice():
 def first_roll_result(dice_sum):
     if dice_sum == 7 or dice_sum == 11:
             print("You win!")
-            return(craps())
+            return "You win!"
     elif dice_sum == 2 or dice_sum == 3 or dice_sum == 12:
             print("You lose")
-            return(craps())
+            return "You lose!"
     else:
         return dice_sum
+def point_roll_result(point_roll):
+    return 1
 #This is the result of when the first roll of the dice happens to either get your point number, a win, or a loss    
 def second_roll_result(dice_sum,point_roll):
     if dice_sum == point_roll:
         print("You won!")
-        return(craps())
+        
     elif dice_sum == 7:
         print("You lose!")
-        return(craps())
+        
     else:
         while (dice_sum != 7 and dice_sum != point_roll):
             dice_sum=roll2dice()

a963c504e767ff036417329280a0a6623ba08d27
ethanriverauag <ethanrivera@uagateway.org>
2017-02-02 17:13:31 +0000

    Added Code

--- unit5/Erivera_scramble.py
+++ unit5/Erivera_scramble.py
@@ -1,10 +1,13 @@
-import random
-#state a sentence that I want to be scrambled
+#state a word that I want to be scrambled
+w = "feeling"
+print (list(w))
+#pick the word and write out a scramble function
 
-sentence = "hello my name is Ethan"
+#turn the letters into a list to be able to be scrambled
+
+#state a sentence that I want to be scrambled
 
 #come up with sentence scramble function
 
+#turn the sentence into a list to be scrambled
 
-random.shuffle(sentence)
-print "sentence"
